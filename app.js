45c3310b8b7a3086dfa5017a3cff3bd5dda0e14d
wero1414 <werowero.edy@gmail.com>
2018-11-01 18:34:21 -0600

    update payload format for catsatzero

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
@@ -15,29 +10,24 @@ var math = require('mathjs');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
-var serialport = require('serialport') //Serial tools
+var serialport = require('serialport')
 var math = require('mathjs')
 
 var stringParse = function(recvString){
 var items = recvString.split(',');
 	return {
-		 id:  items[0]
-		,temp1: items[1]
-		,hum: items[2]
-    ,pres: items[3]
-		,temp2: items[4]
-		,mx: items[5]
-		,my: items[6]
-		,mz: items[7]
-		,ax: items[8]
-		,ay: items[9]
-		,az: items[10]
-		,gx: items[11]
-		,gy: items[12]
-		,gz: items[13]
-		,lat: items[14]
-    ,lon: items[15]
-    ,ssid: items[16]
+    //ID,CO2,TVOC,TEMP,PRESS,ALT,HUM,VOLT,LAT,LONG,SSID
+		 ID:  items[0]
+		,co2: items[1]
+		,tvoc: items[2]
+    ,temp: items[3]
+		,pres: items[4]
+		,alt: items[5]
+		,hum: items[6]
+		,volt: items[7]
+		,lat: items[8]
+		,lon: items[9]
+		,ssid: items[10]
 	}
 }
 
@@ -74,64 +64,52 @@ io.sockets.on('connection', function(socket){
 		socket.broadcast.emit('coords:user', data);
 	});
 
-	//socket.emit('news', { hello: 'world' });
-
 	port.on('data', function(line){
       var today = new Date();
-      var gprmcObj = stringParse(line);
+      var payload = stringParse(line);
 			var pos = {
-				 lat: gprmcObj.lat,
-         lng: gprmcObj.lon
+				 lat: payload.lat,
+         lng: payload.lon
 			};
-
+      //CO2,TVOC,TEMP,PRESS,ALT,HUM,VOLT
       var sen = {
-        temp: gprmcObj.temp1,
-        hume: gprmcObj.hum,
-        press: gprmcObj.pres,
-        temp2: gprmcObj.temp2,
+        co2: payload.co2
+        tvoc: payload.tvoc
+        temp: payload.temp,
+        press: payload.pres,
+        alt: payload.alt,
+        hume: payload.hum,
+        batt: payload.volt
+        ssid: payload.ssid  
 			};
 
-      /* Hyposometric formula:                      */
-      /*                                           */
-      /*     ((P0/P)^(1/5.257) - 1) * (T + 273.15)  */
-      /* h = -------------------------------------  */
-      /*                   0.0065                   */
-      /*                                            */
-      /* where: h   = height (in meters)            */
-      /*        P0  = sea-level pressure (in hPa)   */
-      /*        P   = atmospheric pressure (in hPa) */
-      /*        T   = temperature (in °C)           */
-
-      var seaLevel=1013.25;
-      console.log(((math.pow((seaLevel/sen.press),0.190223)-1.0)*(parseFloat(sen.temp2)+273.15))/0.0065);
-
-    console.log(gprmcObj);
+    console.log(payload);
 		socket.emit('coords:gps', {
 				 latlng: pos
-			}); //emit
+			}); //Envia informacion de coordenadas a frontend
 
-      socket.emit('datos:sensors', {
-  				 sensores: sen
-  			}); //emit
+    socket.emit('datos:sensors', {
+          sensores: sen
+      }); //Envia informacion de sensores a frontend
 
-      fs.open('info.txt', 'wx', (err, fd) => {
-        if (err) {
-          if (err.code === "EEXIST") {
-            fs.appendFile('info.txt', '\n'+ today + line, (err) => {
-              if (err) throw err;
-              console.log('String agregada');
-            });
-            return;
-          } else {
-      throw err;
-    }
+    fs.open('info.txt', 'wx', (err, fd) => {
+      if (err) {
+        if (err.code === "EEXIST") {
+          fs.appendFile('info.txt', '\n'+ today + line, (err) => {
+            if (err) throw err;
+            console.log('String agregada');
+          });
+          return;
+        } else {
+    throw err;
+  }
   }
   fs.writeFile('info.txt', today + line, (err) => {
     if (err) throw err;
     console.log('String guardado');
       });//write file
     });
-	}); //port on
+	}); //Port receive function end
 
 });
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,10 @@
-// @file: main.js
 function onScript(){
 
 	var socket = io.connect(window.location.href);
 
 
 	var mymap = L.map('mimapa',{
-		 center: [19.210216666666668, -96.17367333333334]
+		 center: [21.900850, -102.316342]
 		,zoom:16
 	});
 
@@ -34,28 +33,36 @@ function onScript(){
 		computer.bindPopup("Usuarios aqui");
 	}
 
-var gageTemp	=0;
-var gageHum	= 0;
-var gageTemp2	=0;
-var gagePress	= 0;
+	var gageCO2 = 0;
+	var gageTVOC = 0;
+	var gageTemp = 0;
+	var gagePress = 0;
+	var gageALT = 0;
+	var gageHum = 0;
+	var gageVolt = 0;
+	var gageSSID = 0;
 
 	socket.on('datos:sensors', function (data) {
 		console.log(data);
 		var sen = data.sensores;
-		gageTemp2	= sen.temp2;
 
+		gageCO2 = sen.co2
+		gageTVOC = sen.tvoc
+		gageTemp = sen.temp;
+		gagePress = sen.press;
+		gageALT = sen.alt; 
 		gageHum	= sen.hume;
+		gageVolt = sen.volt;
+		gageSSID = sen.ssid;
 
-		gageTemp	= sen.temp;
-
-		gagePress	= sen.press;
-
+		CO2.refresh(gageCO2);
+		TVOC.refresh(gageTVOC);
 		Temperatura.refresh(gageTemp);
-		Humedad.refresh(gageHum);
-
 		Presion.refresh(gagePress);
-		Temperatura2.refresh(gageTemp2);
-
+		Altitud.refresh(gageALT);
+		Humedad.refresh(gageHum);
+		Voltaje.refresh(gageVolt);
+		SSID.refresh(gageSSID);
 
 	});
 
@@ -69,7 +76,7 @@ var gagePress	= 0;
 
 	}
 
-	var gps = L.marker([19.210216666666668, -96.17367333333334]);
+	var gps = L.marker([21.900850, -102.316342]);
 	mymap.addLayer(gps);
 	var posizioni = [];
 
@@ -84,38 +91,69 @@ var gagePress	= 0;
 
 	});
 
+	var CO2 = new JustGage({
+		id: "gaugeCO2",
+		value: gageCO2,
+		min: -60,
+		max: 90,
+		title: "Co2"
+	});
+
+	var TVOC = new JustGage({
+		id: "gaugeTVOC",
+		value: gageTVOC,
+		min: -60,
+		max: 90,
+		title: "TVOC"
+	});
+
 	var Temperatura = new JustGage({
-		id: "gauge",
+		id: "gaugeTEMP",
 		value: gageTemp,
 		min: -60,
 		max: 90,
 		title: "Temperatura"
 	});
 
-	var Humedad = new JustGage({
-    id: "gauge1",
-    value: gageHum,
-    min: 0,
-    max: 100,
-    title: "Humedad"
-  });
-
 	var Presion = new JustGage({
-    id: "gauge2",
-    value: gagePress,
-    min: 250,
-    max: 1100,
-    title: "Presion"
-  });
+		id: "gaugePres",
+		value: gagePress,
+		min: 250,
+		max: 1100,
+		title: "Presion"
+ 	});
 
-	var Temperatura2 = new JustGage({
-		id: "gauge3",
-		value: gageTemp2,
-		min: -60,
-		max: 90,
+	 var Altitud = new JustGage({
+		id: "gaugeAlt",
+		value: gageALT,
+		min: 0,
+		max: 40000,
+		title: "Altura"
+	 });
+	 
+	var Humedad = new JustGage({
+		id: "gaugeHUM",
+		value: gageHum,
+		min: 0,
+		max: 100,
+		title: "Humedad"
+  	});
+
+	var Voltaje = new JustGage({
+		id: "gaugeVolt",
+		value: gageVolt,
+		min: 0,
+		max: 3,
 		title: "Temperatura 2"
 	});
 
+	var SSID = new JustGage({
+		id: "gaugeSSID",
+		value: gageSSID,
+		min: -130,
+		max: -20,
+		title: "SSID"
+ 	});
 
 }
 
